f35d4681dbbd4343be63a6a6f3ce6f3751131ebd
Κωνσταντίνα Νίκα <kwnstantinanika@gmail.com>
2025-05-14 19:43:31 +0300

    feat

---
+++ src/gr/aueb/cf/helloapp/AddApp.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.helloapp;
+
+public class AddApp {
+
+        public static void  main(String[] args) {
+
+        }
+
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
